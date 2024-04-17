a00fc9bf65ee1fca8cd86eacbf3a05fcb83cc1af
Ihor Rohovets <freego555@gmail.com>
2024-04-17 11:34:23 +0300

    Client. Move configuration to .env file.

--- client/src/config.js
+++ client/src/config.js
@@ -1,17 +1,22 @@
 const config = {
+  backendUrl: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001',
   firebase: {
     // Your web app's Firebase configuration here
     // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'API_KEY',
+    authDomain:
+      process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
+      'PROJECT_ID.firebaseapp.com',
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'PROJECT_ID',
+    storageBucket:
+      process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'PROJECT_ID.appspot.com',
+    messagingSenderId:
+      process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ||
+      'MESSAGING_SENDER_ID',
+    appId: process.env.REACT_APP_FIREBASE_APP_ID || 'APP_ID',
+    measurementId:
+      process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || 'G-MEASUREMENT_ID',
   },
-  backendUrl: 'http://localhost:3001',
 };
 
 export default config;
